934a889de929370cf4f17450088ca0aacba8bb0d
callthecops <tudor_rimbetiu@yahoo.com>
2020-10-13 20:49:16 +0300

    init

---
+++ src/main/java/com/example/BootStrapProject/BootStrapProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.BootStrapProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStrapProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootStrapProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BootStrapProject/BootStrapProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BootStrapProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootStrapProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
